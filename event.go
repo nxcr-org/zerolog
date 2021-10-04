5822b54182dd54236ecbd6c0b8dbc9a9dd65ccd3
Andrew Krueger <andrewk@nxcr.com>
2021-10-04 16:37:09 -0600

    Adds nxcr customizations

--- event.go
+++ event.go
@@ -239,6 +239,34 @@ func (e *Event) EmbedObject(obj LogObjectMarshaler) *Event {
 	return e
 }
 
+func (e *Event) Op(op string) *Event {
+	return e.Str("op", op)
+}
+
+func (e *Event) Route(route string) *Event {
+	return e.Str("route", route)
+}
+
+func (e *Event) CustomerId(customerId string) *Event {
+	return e.Str("customerId", customerId)
+}
+
+func (e *Event) SubscriptionId(subscriptionId string) *Event {
+	return e.Str("subscriptionId", subscriptionId)
+}
+
+func (e *Event) VehicleId(vehicleId string) *Event {
+	return e.Str("vehicleId", vehicleId)
+}
+
+func (e *Event) StatementId(statementId string) *Event {
+	return e.Str("statementId", statementId)
+}
+
+func (e *Event) PaymentId(paymentId string) *Event {
+	return e.Str("paymentId", paymentId)
+}
+
 // Str adds the field key with val as a string to the *Event context.
 func (e *Event) Str(key, val string) *Event {
 	if e == nil {
